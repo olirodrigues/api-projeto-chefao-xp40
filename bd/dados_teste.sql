d90346cfba059fd370877438c7edcb180b9acc86
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-04 20:46:26 -0300

    BD: Adicionado dados ficticios dos endereços

--- bd/dados_teste.sql
+++ bd/dados_teste.sql
@@ -5,3 +5,11 @@ INSERT INTO cliente VALUES
 (null, 'Eliane Silva Sampaio', "sampaeliane@gmail.com", '95577741051', '851232eli', '011992134471' ),
 (null, 'Marta Bertollini Schaffel', "marta95berto@gmail.com", '62135785122', 'schaffelMar85', '051995412233' ),
 (null, 'Eloi Pedro Silveira', "eloi.pedro@gmail.com", '95551457312', '77eloisilveira', '021985553211' );
+
+-- Endereços
+INSERT INTO endereco VALUES
+(null, 'Av', "Martim Ribeiro", 551, "Independência", '77501-100', "Alto Das Flores", "SC", 4),
+(null, 'Av', "Santos Drummond", 1455, "Centro", '01120-085', "São Paulo", "SP", 3),
+(null, 'Rua', "São Félix", 18, "Laranjeiras", '92333-100', "Viamão", "RS", 1),
+(null, 'Av', "Brasil", 2030, "Moinhos de Vento", '40510-950', "Santa Marta", "MG", 2),
+(null, 'Rua', "Hélio Mascarenhas", 45, "Alameda das Flores", '21001-120', "Rio de Janeiro", "RJ", 5);
