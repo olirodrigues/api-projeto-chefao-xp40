f4ef0057710cae0aa19f1a2261caa07899b753cf
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-04 20:30:48 -0300

    BD: Adicionado dados ficticios de clientes

---
+++ bd/dados_teste.sql
@@ -0,0 +1,7 @@
+-- Clientes 
+INSERT INTO cliente VALUES 
+(null, 'Suzane Almeida', "suzaneAl88@gmail.com", '45568778812', 'almeida88suz', '064995919121' ),
+(null, 'Alexandre De Sá', "alesa@outlook.com", '74413255122', 'desaale744', '071988512131' ),
+(null, 'Eliane Silva Sampaio', "sampaeliane@gmail.com", '95577741051', '851232eli', '011992134471' ),
+(null, 'Marta Bertollini Schaffel', "marta95berto@gmail.com", '62135785122', 'schaffelMar85', '051995412233' ),
+(null, 'Eloi Pedro Silveira', "eloi.pedro@gmail.com", '95551457312', '77eloisilveira', '021985553211' );
