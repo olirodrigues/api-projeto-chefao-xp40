45326021b3c7bf58ef74dc2cec4a1e4c32189c16
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-04 21:17:52 -0300

    BD: Adicionado dados ficticios de dptos e produtos

--- bd/dados_teste.sql
+++ bd/dados_teste.sql
@@ -13,3 +13,21 @@ INSERT INTO endereco VALUES
 (null, 'Rua', "São Félix", 18, "Laranjeiras", '92333-100', "Viamão", "RS", 1),
 (null, 'Av', "Brasil", 2030, "Moinhos de Vento", '40510-950', "Santa Marta", "MG", 2),
 (null, 'Rua', "Hélio Mascarenhas", 45, "Alameda das Flores", '21001-120', "Rio de Janeiro", "RJ", 5);
+
+-- Categorias
+INSERT INTO categoria VALUES
+(null, "Suculenta", "Escolha entre as mais variadas espécies de suculentas para decorar sua casa"),
+(null, "Samambaia", "As maiores e belas samambaias para seu ambiente"),
+(null, "Vasos", "Vasos variados para todos os tipos de plantas");
+
+-- Produtos
+
+INSERT INTO produto VALUES
+(null, "Estrelinha Gorda", "Suculenta Estrelinha Gorda", 15, 5, 'estrelinha-gorda.png', 1),
+(null, "Orelha de Shrek ", "Suculenta Orelha de Shrek para acompanhar seu burro", 22, 3, 'orelha-sherek.png', 1),
+(null, "Planta fantasma", "Suculenta Planta fantasma", 18, 4, 'planta-fantasma.png', 1),
+(null, "Samambaia do Sul", "Samambaia do Sul para pendurar no teto", 40, 2, 'samambaia-sul.png', 2),
+(null, "Samambaia Americana", "Samambaia Americana para quintal", 25, 6, 'samambaia-americana.png', 2),
+(null, "Vaso de Samambaia", "Vaso grande de samambaia", 12, 4, 'vaso-samambaia.png', 3),
+(null, "Vaso pequeno", "Vaso tamanho pequeno - marrom", 25, 6, 'vaso-pequeno.png', 3),
+(null, "Vaso de Suculenta", "Vaso de suculenta - preto", 10, 5, 'vaso-suculenta.png', 3);
