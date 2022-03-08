5fbb8bb5ce651c2b7b53ab6d1ab243a5be90e678
Paloma Falchi <palomapfalchi@gmail.com>
2022-03-08 16:51:02 -0300

    BD: tabelas deletadas

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+
+const DB_NOME = "portfolio";
+const DB_USUARIO = "root";
+const DB_SENHA = "";
+const DB_CONFIG = {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+};
+
+let db = {};
+
+try {
+    db = new Sequelize(DB_NOME, DB_USUARIO, DB_SENHA, DB_CONFIG);
+} catch (error) {
+    console.error("Erro ao tentar conexão com o banco de dados");
+}
+
+module.exports = db;
