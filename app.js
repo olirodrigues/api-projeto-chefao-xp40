efb2f2b085150f22ff0f7be3b1f7316759956f04
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-17 22:00:06 -0300

    Feat: alteração na porta para 8000

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const database = require("./src/database");
 const db = require("./src/database");
 
 const app = express();
-
-app.listen(3001, () => console.log("Servidor rodando na porta 3001!"));
+const PORT = 8000;
 
 db.temConexao();
 
+app.listen(process.env.PORT || 8001, () => console.log("Servidor online!"));
 app.use(express.json());
 app.use(routes);
