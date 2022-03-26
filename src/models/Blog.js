8a5a4b11e54c20b8fa06a0bb1d4533bc8cfa0529
WellingtonMax <wellington.psi@hotmail.com>
2022-03-26 18:29:19 -0300

    Feat: Configurando rota do blog

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -1,4 +1,5 @@
 const db = require("../database");
+const FotosGerais = require("./FotosGerais");
 const { DataTypes } = require("sequelize");
 
 const Blog = db.define(
@@ -11,38 +12,18 @@ const Blog = db.define(
     post_titulo: {
       type: DataTypes.STRING,
     },
+    post_resumo: {
+      type: DataTypes.STRING,
+    },
     post_texto: {
       type: DataTypes.STRING,
     },
     post_fotos_id: {
       type: DataTypes.INTEGER,
-    },
-    id: {
-      type: DataTypes.INTEGER,
-    },
-    desktop_1: {
-      type: DataTypes.STRING,
-    },
-    desktop_2: {
-      type: DataTypes.STRING,
-    },
-    desktop_3: {
-      type: DataTypes.STRING,
-    },
-    desktop_4: {
-      type: DataTypes.STRING,
-    },
-    tablet_1: {
-      type: DataTypes.STRING,
-    },
-    tablet_2: {
-      type: DataTypes.STRING,
-    },
-    tablet_3: {
-      type: DataTypes.STRING,
-    },
-    celular_1: {
-      type: DataTypes.STRING,
+      references: {
+        model: FotosGerais,
+        key: "id",
+      },
     },
   },
   {
