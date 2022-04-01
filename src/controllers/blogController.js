e7395cb002376d3deb7bc34d971d9c12b714692a
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-04-01 15:56:09 -0300

    Hotfix: adicionado o campo de id na api

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -3,7 +3,7 @@ const { Blog, FotosGerais } = require("../models");
 const blogController = {
   listarPostsBlog: async (req, res) => {
     const listaPostsBlog = await Blog.findAll({
-      attributes: ["post_titulo", "post_resumo"],
+      attributes: ["id","post_titulo", "post_resumo"],
       include: [
         {model: FotosGerais, attributes: [["capa_blog", "Capa_do_blog"], ["capa_blog_cel", "Capa_do_blog_cel"]]},
       ]
