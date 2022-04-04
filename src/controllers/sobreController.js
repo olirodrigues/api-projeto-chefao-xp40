509bc7bcdb63bcc7637b3105cdf363114552ce8b
WellingtonMax <83736385+WellingtonMax@users.noreply.github.com>
2022-04-03 22:31:12 -0300

    Hotfix: Ajuste das mensagens de erro nos controllers. (#21)

--- src/controllers/sobreController.js
+++ src/controllers/sobreController.js
@@ -1,18 +1,20 @@
 const { Sobre, FotosGerais } = require("../models");
 
 const sobreController = {
-    listarUmPostSobre: async( req, res) =>{
+  listarUmPostSobre: async (req, res) => {
     const listaUmPostSobre = await Sobre.findByPk(req.params.id, {
       include: [{ model: FotosGerais }],
-    })
+    });
 
     if (!listaUmPostSobre) {
-      console.error("Post não encontrado!");
+      res.status(404).json({
+        mensagem:
+          "Ops! Algo deu errado, a página que você tentou acessar não existe.",
+        status: 404,
+      });
     } else if (req.params.categorias == listaUmPostSobre.categoria) {
       res.json(listaUmPostSobre);
-    } 
-  }
-  
+    }
+  },
 };
-
 module.exports = sobreController;
